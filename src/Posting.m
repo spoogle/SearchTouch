1cc4d076e6033fd5c68870207a339f7e4f76b87b
julian.richardson <julian.richardson@gmail.com>
2012-02-21 12:49:32 -0800

    Add description method

--- src/Posting.m
+++ src/Posting.m
@@ -15,4 +15,12 @@
     return [PostingPlaceholder sharedPostingPlaceholder];
 }
 
+- (NSString *)description {
+    NSMutableString *desc = [NSMutableString string];
+    [desc appendFormat:@"DocID:%d, %d positions:", self.docid, self.npostings];
+    for (int i=0; i < self.npostings; i++) {
+        [desc appendFormat:@" %d",(self.positions)[i]];
+    }
+    return [NSString stringWithString:desc];
+}
 @end
